6719fac4c2b0e1d529277e406b12a69aff14754e
choimirae <109408165+choimirae@users.noreply.github.com>
2022-07-16 16:23:31 +0900

    Add files via upload

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ def write(name, desc):
 
 # 하단 버튼 링크 연결 함수
 def button(*args):
-  link = "https://www.naver.com/" # https:// 꼭 붙여야 연결됩니다!
+  link = "https://www.instagram.com/miraexhoi/" # https:// 꼭 붙여야 연결됩니다!
   webbrowser.open(link)
 
 # 배경 색깔 설정 함수
@@ -21,19 +21,19 @@ def information(info):
     write("b" + str(i), info[key[i]])
 
 # 배경 색깔 설정
-colors = ["green", "blue"]
+colors = ["#819FF7", "#81F781"]
 background(colors)
 
 # 이름과 설명, 버튼에 들어갈 글 설정
-write("name", "이름")
-write("description", "설명")
-write("button", "버튼")
+write("name", "최미래")
+write("description", "새론중 3학년")
+write("button", "인스타그램")
 
 # 상세설명에 들어갈 제목과 글 설정
 informations = {
-  "제목1": "내용1",
-  "제목2": "내용2",
-  "제목3": "내용3",
-  "제목4": "내용4"
+  "좋아하는 것": "잠",
+  "학교": "새론중학교",
+  "생일": "20070707",
+  "엠비티아이": "estp"
 }
 information(informations)
